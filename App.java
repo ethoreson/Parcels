9ee7d3b252b304f962391c44bee87a9b333d7d1f
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-27 10:00:17 -0700

    build skeletons of calculating cost methods

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    
+  Console myConsole = System.console();
+
+  System.out.println("~~~~~~~~~~~~~~~~~");
+  System.out.println()
   }
 }
--- Parcel.java
+++ Parcel.java
@@ -14,4 +14,16 @@ class Parcel {
   public int calculateVolume(int length, int width, int height) {
     return (length * width * height);
   }
+
+  public int costToShip(int volume, int weight) {
+    return ((volume / 2) + (weight / 2));
+  }
+
+  public int twoDayShipping(int cost) {
+    return (cost + 6);
+  }
+
+  public int giftWrapped(int cost) {
+    return (cost + 2);
+  }
 }
