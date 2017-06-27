bbe06e5c28281d3b784071ead995ad2fecbb8b5a
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-27 09:44:27 -0700

    create parcel class

--- App.java
+++ App.java
@@ -1,3 +1,9 @@
 import java.io.Console;
 import java.util.Array;
 import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    
+  }
+}
--- Parcel.java
+++ Parcel.java
@@ -1,3 +1,17 @@
 class Parcel {
-  
+  public int mLength;
+  public int mWidth;
+  public int mHeight;
+  public int mWeight;
+
+  public Parcel(int length, int width, int height, int weight) {
+    mLength = length;
+    mWidth = width;
+    mHeight = height;
+    mWeight = weight;
+  }
+
+  public int calculateVolume(int length, int width, int height) {
+    return (length * width * height);
+  }
 }
